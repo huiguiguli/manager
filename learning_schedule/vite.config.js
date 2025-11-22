151f5ff88e98ba3d2172296ade0c5b3bf1ac4843
回归故里 <3229367985@qq.com>
2025-11-22 14:18:01 +0800

    manager

--- learning_schedule/vite.config.js
+++ learning_schedule/vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/manager/' // 关键！填 Gitee 项目的名称（小写，与仓库名一致）
+  base: './', // 必须是 ./ ！相对路径
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
